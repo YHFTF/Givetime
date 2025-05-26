54196a7156e7fe2632a7076154416ede645ebc03
YHFTF <yunghy1@naver.com>
2025-05-26 20:34:37 +0900

    로그인 모달 header에 편입 및 js 통일

--- main/static/main/main.js
+++ main/static/main/main.js
@@ -23,34 +23,3 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   }, 4000);
 });
-
-// ✅ 로그인 모달 열고 닫기
-function openLoginModal() {
-  document.getElementById("loginModal").style.display = "flex";
-  document.getElementById("signupModal").style.display = "none";
-}
-
-function closeLoginModal() {
-  document.getElementById("loginModal").style.display = "none";
-}
-
-// ✅ 회원가입 모달 열고 닫기
-function openSignupModal() {
-  document.getElementById("signupModal").style.display = "flex";
-  document.getElementById("loginModal").style.display = "none";
-}
-
-function closeSignupModal() {
-  document.getElementById("signupModal").style.display = "none";
-}
-
-// ✅ 비밀번호 토글
-function togglePassword() {
-  const pwd = document.getElementById("password");
-  pwd.type = pwd.type === "password" ? "text" : "password";
-}
-
-function toggleConfirmPassword() {
-  const pwd = document.getElementById("confirmPassword");
-  pwd.type = pwd.type === "password" ? "text" : "password";
-}

557a5590a65101b5a370ff24b2829aeec4b35245
박영현 <92726344+YHFTF@users.noreply.github.com>
2025-06-08 15:22:53 +0900

    마이페이지에 관리자 등록 기능 추가

--- mypage/urls.py
+++ mypage/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('profile/<str:nickname>/', views.view_profile, name='view_profile'),  # 닉네임 기반 프로필 보기
     path('update/<int:user_id>/', views.update_profile, name='update_profile'),  # 프로필 수정
     path('search/', views.search_user, name='search_user'),  # 닉네임 검색
+    path('register-admin/', views.register_admin, name='register_admin'),  # 관리자 등록
 ]
